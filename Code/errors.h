dcc06434fb280e954d30c2d9a99240f46dc8e6ff
claudiaquera01 <72881206+claudiaquera01@users.noreply.github.com>
2024-01-31 12:25:17 +0100

    Update errors.h

--- Code/errors.h
+++ Code/errors.h
@@ -3,6 +3,7 @@
 
 //Error codes at Main
 #define MAIN_ERROR_CANT_READ_FILE 1
+#define MAIN_ERROR_MISSING_FILENAME 2
 
 // Error codes at SCANNER
 #define SCANNER_SUCCESS 0
@@ -10,10 +11,10 @@
 
 //Error messages at MAIN
 extern const char* ERROR_MESSAGE_CANT_READ_FILE;
+extern const char* ERROR_MESSAGE_MISSING_FILENAME;
 
 // Error messages at SCANNER
 extern const char* ERROR_MESSAGE_SCANNER_SUCCESS;
 extern const char* ERROR_MESSAGE_RROR_NONALPHABETCHAR;
-
 
 #endif

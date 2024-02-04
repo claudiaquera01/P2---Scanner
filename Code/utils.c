2177285b6a5275cd5b7e98fa5f398ad3d23d89ea
MAranich <114926980+MAranich@users.noreply.github.com>
2024-02-04 20:01:27 +0100

    sort chars

--- Code/utils.c
+++ Code/utils.c
@@ -1,2 +1,25 @@
 #include "utils.h"
 
+
+
+
+
+
+
+
+void sort_chars(char* array, int len) {
+    // the trustworthy bubblesort
+    char temp;
+    for (int i = 0; i < len - 1; i++) {
+        for (int j = 0; j < len - i - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                //swap
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+
+}
+
--- Code/utils.h
+++ Code/utils.h
@@ -5,8 +5,21 @@
 
 
 
+/*
+Sorts the caracters in the array. len is the length of the array. 
 
+Note that array needs to be mutable, therefore using the following will not work: 
 
+    char* my_text = "cute reflection"; 
+    sort_chars(my_text, strlen(my_text));
+
+To easly fix it, substitute by the following. 
+
+    char my_text[] = "cute reflection"; 
+    sort_chars(my_text, strlen(my_text));
+
+*/
+void sort_chars(char* array, int len); 
 
 
 

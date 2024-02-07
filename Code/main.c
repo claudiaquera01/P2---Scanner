986799eb2712092baf42e397750ce8147c5bf907
MAranich <114926980+MAranich@users.noreply.github.com>
2024-02-07 10:17:50 +0100

    added comments and small changes

--- Code/main.c
+++ Code/main.c
@@ -4,8 +4,8 @@
 // Function to process a C file character by character
 char* processFile(const char* filename) {
     // Open the file
-    FILE* file = fopen(filename, "r");
-    if (file == NULL) {
+    FILE* file = fopen(filename, "rb");
+    if (file == NULL) { 
         fprintf(stderr, "Error opening file: %s\n", filename);
         return NULL;
     }
@@ -24,9 +24,24 @@ char* processFile(const char* filename) {
     // Read the file character by character
     while ((currentChar = getc(file)) != EOF) {
         // Process the current character
+
+        // TODO: create all DFAs, initialize them, advance the dfa with the new char
+        // if next char is a delimiter, finalize all dfas and add the token to the result
+        // if current character is a delimiter, then we also finish all the dfas
+        // this way the contents "x * y" will be parsed as "x| |*| |y"
+
+
+
+
         // For now, let's just print each character
         printf("%c", (char)currentChar);
-    }
+    } 
+
+
+
+    //TODO: redo ^this. We need to write then the buffer is full and start again with a new empty buffer
+
+    //End all dfas
 
     // Close the file
     fclose(file);
@@ -45,6 +60,7 @@ int main(int argc, char *argv[]) {
 
     // Call the processFile function with the filename
     char* resultBuffer = processFile(argv[1]);
+
 
     if (resultBuffer == NULL) {
         fprintf(stderr, "Error: %s\n", ERROR_MESSAGE_FILE_PROCESSING);

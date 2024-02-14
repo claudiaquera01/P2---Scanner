9b2f2e429120f674729fc8e4d8c045795cfc9314
AriadnaPrat <114828871+AriadnaPrat@users.noreply.github.com>
2024-02-14 14:00:35 +0100

    Update debug.c

--- Code/debug.c
+++ Code/debug.c
@@ -1,53 +1,31 @@
 #include "debug.h"
 
-// Function to output tokens in debug mode
-void output_tokens_debug(FILE* output_file, Token* tokens, int num_tokens, int debug_mode) {
+int num=0;
+char* debug_mode(char* element , int identificator, int len, int debug_mode) {
     // Write tokens in the output version
-    int num;
-    num=0;
-    for (int i = 0; i < num_tokens; i++) {
-        if(num!=tokens[i].line_number){
-            //fprintf(output_file, "\n");
-            printf("\n");
-            if (debug_mode == DEBUG_ON) {
-                //fprintf(output_file, "%d ", tokens[i].line_number);
-                printf("%d ", tokens[i].line_number);
-            }
-        }
-
-        const char* category_str;
-        switch (tokens[i].category) {
-            case CAT_NUMBER:
-                category_str = "CAT_NUMBER";
-                break;
-            case CAT_IDENTIFIER:
-                category_str = "CAT_IDENTIFIER";
-                break;
-            case CAT_KEYWORD:
-                category_str = "CAT_KEYWORD";
-                break;
-            case CAT_LITERAL:
-                category_str = "CAT_LITERAL";
-                break;
-            case CAT_OPERAND:
-                category_str = "CAT_OPERAND";
-                break;
-            case CAT_SPECIALCHAR:
-                category_str = "CAT_SPECIALCHAR";
-                break;
-            case CAT_NONRECOGNIZED:
-                category_str = "CAT_NONRECOGNIZED";
-                break;
-            default:
-                category_str = "UNKNOWN";
-                break;
-        }
-
-        //fprintf(output_file, "<%s, %s>", tokens[i].lexeme, category_str);
-        printf("<%s, %s>", tokens[i].lexeme, category_str);
-        //Add a line break after each token
-        if (num!=tokens[i].line_number){
+    char* token=(char*)malloc(sizeof(char) * (len+4));
+    if(debug_mode=DEBUG_ON){
+        if(num==START){
+            printf("\n %d ",num);
+            sprintf(token, "\n %d",num);
             num++;
+            COUNTFUNC(2*PRINTF_COST+ARITHMETIC_COST);
         }
+        if(strcmp(DEBUG_KEY,element)==0){
+            token=tokenize(identificator, element, len);
+            printf("\n %d ",num);
+            sprintf(token, "\n %d",num);
+            num++;
+            COUNTFUNC(2*PRINTF_COST+ARITHMETIC_COST);
+        }else{
+            token=tokenize(identificator, element, len);
+        }
+        COUNTFUNC(2*IF_COST);
+    }else{
+        token=(char*)malloc(sizeof(char) * (len));
+        token=tokenize(identificator, element, len);
+        COUNTFUNC(ARITHMETIC_COST);
     }
+    COUNTFUNC(ARITHMETIC_COST+RETURN_COST+IF_COST);
+    return token;
 }

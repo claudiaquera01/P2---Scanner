df38a361e7fd62b3df7a2125a1233660109a03fa
AriadnaPrat <114828871+AriadnaPrat@users.noreply.github.com>
2024-02-14 21:45:40 +0100

    Update debug.h

--- Code/debug.h
+++ Code/debug.h
@@ -6,10 +6,12 @@
 #define DEBUG_OFF 0
 #define DEBUG_KEY "\n"
 #define START 0
+extern int debug;
 extern int num;
 #include "datastructures.h"
 #include "utils.h"
 // Functions
 /* We use the function tokenize to get the token but we can apply the normal mode, or the debug mode*/
-char* debug_mode(int debug_mode; char* element , int identificator, int len, int debug_mode);
+//char* debug_mode(int debug_mode; char* element , int identificator, int len, int debug_mode);
+char* debug_mode(const char* element, int len, const char* token_identifier);
 #endif /* DEBUG_H */

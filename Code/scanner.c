f208108db727c807c76eb9d442495c6e953dd734
MAranich <114926980+MAranich@users.noreply.github.com>
2024-02-15 17:29:51 +0100

    deleted useless files, turned off debug mode

--- Code/scanner.c
+++
@@ -1,4 +0,0 @@
-#include "scanner.h"
-
-/* In this file we will implement the scanner logic using a finite automaton 
-to recognize different token categories.*/ // <- DFA already implemented in datastructures.h
--- Code/scanner.h
+++
@@ -1,8 +0,0 @@
-#ifndef SCANNER_HEADER 
-#define SCANNER_HEADER
-
-#include "errors.h"
-
-
-
-#endif

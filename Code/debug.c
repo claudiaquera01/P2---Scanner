3b51c43e01069cfa8094b714f790fd539fd53ed2
AriadnaPrat <114828871+AriadnaPrat@users.noreply.github.com>
2024-02-15 20:21:57 +0100

    Update debug.c

--- Code/debug.c
+++ Code/debug.c
@@ -39,37 +39,7 @@ char* debug_mode(const char* element, int len, const char* token_identifier) {
         //num++;
         COUNTFUNC(ARITHMETIC_COST + PRINTF_COST + 2*IF_COST);
     } else {
-        COUNTFUNC(3*ARITHMETIC_COST + PRINTF_COST + IF_COST);
-
-        if(strcmp(DEBUG_KEY, element) == 0){
-            //sprintf(token, "\n ");  // Add the number before the token
-            strcat(token, "\n ");
-        } 
-        
-        char* aux = generate_token(element, len, token_identifier); 
-
-        strcat(token, aux); 
-        //free(aux); 
-        // ^freeing this is giving an error and is driving me crazy
-        aux = generate_token(element, len, token_identifier); 
-        //strcat(token, aux); 
-
-
-        //strcat(token, generate_token(element, len, token_identifier));
-
-        //sprintf(token, " ");  // Add space to avoid rare characters 
-
-        
-        /*
-        char* aux = generate_token(element, len, token_identifier); 
-        strcat(token, aux);
-        free(aux); 
-
-        sprintf(token, " ");  // Add space to avoid rare characters 
-
-        aux = generate_token(element, len, token_identifier); //I do not understand why we need to generate token twice, but i freed the requested memory
-        strcat(token, aux);
-        free(aux); */
+        token=generate_token(element, len, token_identifier);
         
     }
 
